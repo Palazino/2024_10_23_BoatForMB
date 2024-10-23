2665bf4cdab41bbd7f543eee0cce7723f53d0a05
BeiNicolas <bei.nicolas8484@gmail.com>
2024-10-23 16:38:58 +0200

    Save MoveBoatBasicAdd

--- Runtime/BasicBoatMono.cs
+++ Runtime/BasicBoatMono.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using TreeEditor;
 using UnityEngine;
 
-namespace BeiNico 
-{ 
+namespace BeiNico
+{
     public class BasicBoatMono : MonoBehaviour
     {
+
+        [Range(-1f, 1f)]
+        [SerializeField] public float m_percentHorizontalAxis;
+        [Range(-1f, 1f)]
+        [SerializeField] public float m_percentVerticalAxis;
+
+        public float moveSpeed = 10f; 
+        public float turnSpeed = 5f; 
+
+        void Update()
+        {
+            MOVE();
+        }
+
+        public void SetHorizontal(float percent)
+        {
+            percent = Mathf.Clamp(percent, -1, 1);
+            m_percentHorizontalAxis = percent;
+        }
+        public void SetVertical(float percent)
+        {
+            percent = Mathf.Clamp(percent, -1, 1);
+            m_percentVerticalAxis = percent;
+
+        }
+
+        private void MOVE()
+        {
+            
+            float moveDirection = m_percentVerticalAxis * moveSpeed * Time.deltaTime;
+            transform.Translate(Vector3.forward * moveDirection);
+
+           
+            float turnDirection = m_percentHorizontalAxis * turnSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up * turnDirection);
+        }
+
+
+
         [ContextMenu("Teleport Forward")]
         public void TeleportForward()
         {
             transform.position += transform.forward * 1;
         }
+
     }
 }
